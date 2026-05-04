1eadd358af145a0a1618d1ea1a9b68df17290b33
Yorich1 <ambassadorcum@gmail.com>
2026-05-04 10:22:05 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,10 @@
+with open("test.py", "r", encoding="utf-8") as f:
+    lines = f.readlines()
 
+with open("test.py", "w", encoding="utf-8") as f:
+    for line in lines:
+        if not line.startswith("#"):
+            f.write("#" + line)
+        else:
+            f.write(line)
+print('Готово')
