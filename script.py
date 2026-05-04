f41e0307f27ad8d17e18bdf8bbe2fed80896a98d
Yorich1 <ambassadorcum@gmail.com>
2026-05-04 10:22:46 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
-with open("test.py", "r", encoding="utf-8") as f:
+with open("test.py", "r", encoding="utf-8") as f:  #Вместо test.py пишешь название своего файла
     lines = f.readlines()
 
-with open("test.py", "w", encoding="utf-8") as f:
+with open("test.py", "w", encoding="utf-8") as f:  #Вместо test.py пишешь название своего файла
     for line in lines:
         if not line.startswith("#"):
             f.write("#" + line)
